2dec6d8d99afa870401609acb15fac6c4af3fde8
devGW <doscmDev@gmail.com>
2019-04-21 14:13:55 +0900

    Refac: 정렬 방식 변경

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
   //render : componentWillMount() -> render() -> componentDidMount()
   //update : componentWillReceiveProps() -> shouldComponentUpdate -> componentWillUpdate() -> render() -> component
-  state = {
-
-
-  }
+  state = {};
 
   componentDidMount() {
-    this._getMovie()
+    this._getMovie();
   }
-  
 
   _getMovie = async () => {
-    const movies = await this._callApi()
+    const movies = await this._callApi();
     this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count') // 작업한것이 then 으로 넘어감
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    ) // 작업한것이 then 으로 넘어감
       .then(response => response.json()) // 작업한것이 다음 then으로 넘어감
       .then(json => json.data.movies)
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
   _renderMoive = () => {
     var movies = this.state.movies.map(movie => {
       return (
-        <Movie 
+        <Movie
           title={movie.title}
           poster={movie.medium_cover_image}
           key={movie.id}
           genres={movie.genres}
           synopsis={movie.synopsis}
         />
-      )
-    })
-    return movies
-  }
+      );
+    });
+    return movies;
+  };
   render() {
     //render 의 state 가 변경되면 자동으로 render 한번 더 실행
     return (
@@ -49,9 +47,7 @@ class App extends Component {
       //jsx 는 리액트 컴포넌트를 만들때 사용하는 언어
       <div className={this.state.movies ? "App" : "App--loading"}>
         {this.state.greeting}
-        {
-          this.state.movies ? this._renderMoive() : "Loadig ..."
-        }
+        {this.state.movies ? this._renderMoive() : "Loadig ..."}
       </div>
     );
   }
